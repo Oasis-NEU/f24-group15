fab18c1c333fb9a6ce8b24b40b00293e4f7bf4d3
Aidan <aidanwls@gmail.com>
2024-10-20 13:38:48 -0400

    Add basic dropdown for classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialList = [
+  { id: 0, title: 'CS1200', done: false },
+  { id: 1, title: 'CS1210', done: false },
+  { id: 2, title: 'CS2500', done: false },
+  { id: 3, title: 'CS2510', done: false},
+];
+
+export default function CourseList() {
+  const [list, setList] = useState(
+    initialList
+  );
+
+  function handleToggle(courseId, nextSeen) {
+    setList(list.map(course => {
+      if (course.id === courseId) {
+        return { ...course, seen: nextSeen };
+      } else {
+        return course;
+      }
+    }));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='title'>
+      <h1>NEU DegreeWorks</h1>
+      <h2>CS requirements:</h2>
+      <ItemList
+        courses={list}
+        onToggle={handleToggle} />
+    </div>
+  );
 }
 
-export default App
+function ItemList({ courses, onToggle }) {
+  return (
+    <ul className="classes">
+      {courses.map(course => (
+        <li key={course.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={course.seen}
+              onChange={e => {
+                onToggle(
+                  course.id,
+                  e.target.checked
+                );
+              }}
+            />
+            {course.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
